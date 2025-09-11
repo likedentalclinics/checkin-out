6095f82da277a29b5c9ce7dae08676e29f0f5f00
likedentalclinics <tgstrider@hotmail.com>
2025-09-11 17:34:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+
+
+// ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
+const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwYo9qOk7R_iBFmMEWO0gFPiOIvq7hQpsDkoetf2chjCQkftsl82N8FpsK_ckNvAZ_Abw/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyBLeHixUu4fX9NcrOXW5nkDP16_7Shw3USixdJdSRJ24oprJc8SrxKen36Fn20CKbF/exec';
+const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbxxyBSvWu_2Z7dW3hB4rR2GlI_B_7t_upHy3FmtAYoHAfXQ4XjLo4jxIhfQPl0lNVfL/exec';
+const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbx62yYUSKxtX_V_D9zdEG8ibZ4IWh6jiAE3VTOmtuOroddsGVE8Kh8M_O0el_n6t_E/exec';
+const CONDUCT_URL = 'https://script.google.com/macros/s/AKfycbxJz3DSvUpX5O7erZHVS95vuByYGLQGSvVdgBQiho7Xw0fItJ6wErovmwSfdFESZpc1Tg/exec';
+
+//LIFF_ID
+const LIFF_ID = '2007036933-DpZar7MN'; // LIFF ID Checkin
+const LIFF_ID1 = '2007036933-04QoEaY2'; // LIFF ID history
+const LIFF_ID2 = '2007036933-2JV0E1oK'; // LIFF ID leave
+const LIFF_ID3 = '2007036933-4n1oYOgj'; // LIFF ID ot
+const LIFF_ID4 = '2007036933-O5ERra5l'; // LIFF ID register
