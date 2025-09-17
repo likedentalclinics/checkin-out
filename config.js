ba0a257c8cefbe7448086fd6162d7c447507a973
likedentalclinics <tgstrider@hotmail.com>
2025-09-17 23:37:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwYo9qOk7R_iBFmMEWO0gFPiOIvq7hQpsDkoetf2chjCQkftsl82N8FpsK_ckNvAZ_Abw/exec';
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyBLeHixUu4fX9NcrOXW5nkDP16_7Shw3USixdJdSRJ24oprJc8SrxKen36Fn20CKbF/exec';
-const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbxxyBSvWu_2Z7dW3hB4rR2GlI_B_7t_upHy3FmtAYoHAfXQ4XjLo4jxIhfQPl0lNVfL/exec';
+const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwasY4SyJlzQwUqABI9sIFT15ARvpIo2ky8xAUXtV4ShBkt_Jqgx8xIj74Bv1Od7EisPQ/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyYyHe0FnJhDPJ3BqRflowc9mYtu5i4D86TlW1q4XEkQ2RQg1dLHdQXFAALkDwX5rkW/exec';
+//const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbxxyBSvWu_2Z7dW3hB4rR2GlI_B_7t_upHy3FmtAYoHAfXQ4XjLo4jxIhfQPl0lNVfL/exec';
 const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbzqBtSX_50D61DIn4aYHeL75vTnQKlwiW0Zlu-CpGRh0N26tWxyb5DnlLgCV7ZUiT6glg/exec';
 const CONDUCT_URL = 'https://script.google.com/macros/s/AKfycbxJz3DSvUpX5O7erZHVS95vuByYGLQGSvVdgBQiho7Xw0fItJ6wErovmwSfdFESZpc1Tg/exec';
 
