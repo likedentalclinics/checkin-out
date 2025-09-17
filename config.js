c7baa3fa113de50ba7c0b4b7c70791bd6c95bc1c
likedentalclinics <tgstrider@hotmail.com>
2025-09-17 23:14:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwYo9qOk7R_iBFmMEWO0gFPiOIvq7hQpsDkoetf2chjCQkftsl82N8FpsK_ckNvAZ_Abw/exec';
 const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyBLeHixUu4fX9NcrOXW5nkDP16_7Shw3USixdJdSRJ24oprJc8SrxKen36Fn20CKbF/exec';
 const WEB_APP_Leave_URL = 'https://script.google.com/macros/s/AKfycbxxyBSvWu_2Z7dW3hB4rR2GlI_B_7t_upHy3FmtAYoHAfXQ4XjLo4jxIhfQPl0lNVfL/exec';
-const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbx62yYUSKxtX_V_D9zdEG8ibZ4IWh6jiAE3VTOmtuOroddsGVE8Kh8M_O0el_n6t_E/exec';
+const WEB_APP_OT_URL = 'https://script.google.com/macros/s/AKfycbzqBtSX_50D61DIn4aYHeL75vTnQKlwiW0Zlu-CpGRh0N26tWxyb5DnlLgCV7ZUiT6glg/exec';
 const CONDUCT_URL = 'https://script.google.com/macros/s/AKfycbxJz3DSvUpX5O7erZHVS95vuByYGLQGSvVdgBQiho7Xw0fItJ6wErovmwSfdFESZpc1Tg/exec';
 
 //LIFF_ID
